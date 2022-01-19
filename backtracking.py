a898a3bfe14850acb43572b57393dca92657a517
Ashkan Vedadi <ashkan.vedadi@live.com>
2022-01-19 22:11:48 +0330

    backtracking

--- backtracking.py
+++ backtracking.py
@@ -1,60 +1,60 @@
 
 
-
-
-include = [0,0,0]
-
-
-def promising (i, weight, profit, n, W, w, p):
-    #j k
-    #totweight
-    #float bound
-    j = 0
-    k = 0
-    
-    if (weight >= W):                           
-        return False                  
-    else:                                 
-        j = i + 1                              
-        bound = profit                         
-        totweight = weight
-        while ((j < n) and (totweight + w[j] <= W)):
-            print(len(w),len(p),j)
-            totweight = totweight + w[j]               
-            bound = bound + p[j]
-            j = j + 1
-            
-        k = j-1                                 
-        if k<=n:                              
-            bound = bound + (W - totweight) * p[k]/w[k]
-
-        return bound > maxprofit                           
-   
-
-
-numbest = 0
-maxprofit = 0
-def knapSackBackTracking(i, profit, weight, W, maxprofit, n, w, p):
-    if weight <= W and profit > maxprofit:
-        maxprofit = profit                  #   so far.
-        #bestset = include
-    if promising(i,weight, profit, n, W, w, p):
-         #include[i + 1] = "yes"      (i, profit, weight, W, maxprofit, n, w, p)
-         knapSackBackTracking(i + 1, profit + p[i + 1], weight + w[i + 1], W, maxprofit, n, w, p) # take it
-         #include[i + 1] = "no"              
-         knapSackBackTracking(i + 1, profit, weight, W, maxprofit, n, w, p) # do not take it
-    return maxprofit
-
-print("Backtracking:")
-
-
-
 p = [60, 100, 120]
 w = [10, 20, 30]
 W = 50
-n = len(p)
-print( knapSackBackTracking(0, 0, 0, W, 0, n, w, p) )
+n = len(p)-1
+numbest = 0
+maxprofit = 0
 
-# for j in numbest:                           #   Show an optimal set of items.
-#     print(bestset[j])
+weight = 0
+profit = 0
 
+def knapsack ( i, profit, weight):
+    global maxprofit
+
+    if (weight <= W and profit > maxprofit):
+        maxprofit = profit                  
+        #numbest = i;                         
+        #bestset = include;                   
+                                            
+    if (promising(i)):
+        #include [i + 1] = "yes"             
+        knapsack(i + 1, profit + p[i + 1], weight + w[i + 1])
+        #include [i + 1] = "no"              
+        knapsack (i + 1, profit, weight)
+     
+   
+
+def promising (i):
+    global weight
+    global profit
+    #j, k
+    #int totweight
+    #  float bound;
+
+    if (weight >= W):                           
+        return False                       
+    else:                                    
+        j = i + 1                              
+        bound = profit                        
+        totweight = weight
+        while (j <= n and totweight + w[j] <= W):
+            totweight = totweight + w[j]         
+            bound = bound + p[j]
+            j = j+1
+    k = j;                                  
+    if (k <= n):                          
+        bound = bound + (W - totweight)*p[k]/w[k]
+                                             
+
+    return bound > maxprofit;                           
+   
+
+
+
+
+knapsack(0, 0, 0)
+print(maxprofit)                           
+# for (j = 1; j <= numbest; j++)                // Show an optimal set of items.
+#       cout << bestset[i]
